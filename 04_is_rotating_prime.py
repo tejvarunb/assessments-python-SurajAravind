44230bb0da786ffa1083455a227188c53f791ac1
Suraj <sbollapr@gitam.edu>
2022-02-17 15:44:41 +0530

    completed program 4

--- 04_is_rotating_prime.py
+++ 04_is_rotating_prime.py
@@ -22,10 +22,42 @@ Although 19 is prime, 91 is not.
 """
 
 
+## Code taken from Thrigun Meduri
+
 # Implement the below function and run the program
 
 def is_rotating_prime(num):
-    pass
+    x=list(str(num))
+    flag=False
+    flag1=[]
+    flage2=False
+    for i in range(len(x)):
+        t=x.pop()
+        x.insert(0, t)
+        y=""
+        for ele in x:
+            y+=ele
+        z=int(y)
+        if z>1:
+            for i in range(2, z):
+                if(z%i==0):
+                    flag=True
+                    break
+        if flag:
+            flag1.append(False)
+        else:
+            flag1.append(True)
+    for i in flag1:
+        if i==True:
+            flag2=True
+            continue
+        else:
+            flag2=False
+            break
+    if flag2:
+        return True
+    else:
+        return False
 
 
 class TestIsRotatingPrime(unittest.TestCase):
